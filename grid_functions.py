87f071064d9c4f51bc69f392b529f191219dfa1b
Rachel Townson <racheltownson24@gmail.com>
2021-03-16 23:12:39 -0400

    Update grid_functions.py

--- grid_functions.py
+++ grid_functions.py
@@ -33,9 +33,8 @@ def create_level(n, s):
     return lvl
 
 
-def get_music(choose, lvls_in_game=5):
+def get_music(choose="", lvls_in_game=5, song="", lvl=0, trial="0"):
     ''' Choosing music for each round of the game'''
-    out_dict = {}
     mus_dict = {"citylife": "https://www.free-stock-music.com/music/artificial-music-city-life.mp3",
                 "shortbreak": "https://www.free-stock-music.com/music/glitch-short-break.mp3",
                 "stoned": "https://www.free-stock-music.com/music/alexander-nakarada-stoned.mp3",
@@ -44,16 +43,28 @@ def get_music(choose, lvls_in_game=5):
                 "vintageradiostation": "https://www.free-stock-music.com/music/sakura-hz-vintage-radio-station.mp3",
                 "midnightdrive": "https://www.free-stock-music.com/music/inossi-midnight-drive.mp3"
                 }
-    if (choose == "same"):  # all rounds with the same song
+    a_list = ["agreatcalm", "citylife", "stoned", "vintageradiostation"]
+    b_list = ["kaleidoscope", "midnightdrive", "melancholia", "spiritcross"]
+    if trial == "4": # changing tempo
+        tempo = 0
+        if choose == "even":
+            tempo = 10 * lvl - 30
+        elif choose == "odd":
+            tempo = -10 * lvl + 30
+        mus = mus_dict[song] + "_" + str(tempo) + ".mp3"
+        song += " " + str(tempo)
+        return song, mus
+    elif trial == "3":  # choose random song for each round
         mus_key = random.choice(list(mus_dict.keys()))
-        for i in range(1, lvls_in_game + 1):
-            out_dict[i] = (mus_key, mus_dict[mus_key])
-    if (choose == "random"):  # choose random song for each round
-       mus_key = random.choice(list(mus_dict.keys()))
-       return (mus_key, mus_dict[mus_key])
-    else: # choosing by name, all rounds with same song
-        mus_key = choose
-        for i in range(1, lvls_in_game + 1):
-            out_dict[i] = (mus_key, mus_dict[mus_key])
-            
+        return mus_key, mus_dict[mus_key] + ".mp3"
+    elif trial == "2":
+        if choose == "even":
+            mus_key = random.choice(a_list)
+            return mus_key, mus_dict[mus_key] + ".mp3"
+        if choose == "odd":
+            mus_key = random.choice(b_list)
+            return mus_key, mus_dict[mus_key] + ".mp3"
+    elif trial == "1":
+        return "none", str()
+
             
